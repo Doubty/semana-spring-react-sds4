85b261b18df9c3b76948109f2d5fe2b7b37668fe
galvs <goantonio80@gmail.com>
2021-09-08 13:14:43 -0300

    Adicionado o bootstrap

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h2 className="text-primary">ola a todos</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
